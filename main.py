dabb128453ca5c5df5773c0e85b4b6d5f6f5b7d0
Yang Zhifei <xividyzf@gmail.com>
2014-07-05 21:28:07 +0800

    优化的接口
    返回字典列表：
                result is a list of dictionaries:
                    result[i]['Name']: app name
                    result[i]['Comment']: app brief introduction
                    result[i]['Exec']: app launch method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions import *
 import subprocess, webbrowser
+
 #Init & Start-up info
 searcher = Searcher()
 print "Linux Quick Launcher"
@@ -22,32 +23,11 @@ while True:
         else: #result ranges from 0 to length-1
              length = len(launch_list)
              for i in range(0, length):
-                 print str(i) + ". " + launch_list[i][1]
+                 print str(i) + ". " + launch_list[i]['Name']
+                 print '  ' + launch_list[i]['Comment']
              choice = eval(raw_input())
              if choice >= 0:
-                 print 'will run:' + launch_list[choice][0]
-                 subprocess.Popen(launch_list[choice][0])
+                 print 'will execute:' + launch_list[choice]['Exec']
+                 subprocess.Popen(launch_list[choice]['Exec'])
     #console display
 
-
-# while True:
-#     kw = raw_input()
-#     if kw == "":
-#         continue
-#     if kw == ":q":
-#         print "Thanks for using. Bye."
-#         exit()
-#
-#     rekw = kw.replace("*", ".*")
-#     rekw = ".*" + rekw.replace("?", ".") + ".*" #replace with regular expressions
-#     result = sorted([[namelist[i],  applist[i]] for i in range(len(namelist)) if re.match(rekw.lower(), namelist[i].lower())])
-#
-#     if result == []:
-#         print "No app found!"
-#     else: #result ranges from 0 to length-1
-#         length = len(result)
-#         for i in range(0, length):
-#             print str(i) + ". " + result[i][0]
-#         choice = eval(raw_input())
-#         if choice >= 0:
-#             subprocess.Popen(result[choice][1])
