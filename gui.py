2d718d1d793770cec18c9cfe2cfe88c4151dc820
Xwz7 <Xwszc7@Gmail.com>
2014-07-05 21:30:35 +0800

    make it..less bugs

--- gui.py
+++ gui.py
@@ -1,50 +1,63 @@
 from Tkinter import *
-import os
-import re
+from functions import *
 import subprocess
-
-def init():
-    global namelist, applist
-    namelist = sorted([app for app in os.listdir("/usr/share/applications") if app[-8:] == ".desktop"])
-    applist = [app[:-8] for app in namelist]
-    getname(namelist)
-
-def getname(l):
-    i = 0
-    for app in l:
-        fp = open("/usr/share/applications/" + app,  "r")
-        name = "init"
-        flag = False
-
-        while name:
-            name = fp.readline()
-            if name[:4] == "Name":
-                flag = True
-                break
-
-        name = name[name.find("=") + 1:-1] if flag else ""
-        l[i] = name
-
-        fp.close()
-        i += 1
 
 def run(app):
     subprocess.Popen(app,  shell = True)
+
+def select(event):
+    global entry_text, flag
+    print apps.curselection(), event.keysym
+    print flag
+    if event.keysym == "Return" and sum != 0:
+        subprocess.Popen(result[apps.curselection()[0]]["Exec"], shell = True)
+        return
+    elif event.keysym == "Escape":
+        entry.focus_set()
+        entry_text.set("")
+        search()
+    elif event.keysym == "Up" and (flag or sum == 1):
+        entry.focus_set()
+    elif event.keysym == "Up" and apps.curselection()[0] == 0:
+        flag = True
+        return
+        
+    flag = False
+
+def input(event):
+    global result, apps, entry_text, sum
+    print event.keysym, event.keycode
+    if event.keysym == "Return" and sum != 0:
+        subprocess.Popen(result[0]["Exec"], shell = True)
+        return
+    elif event.keysym == "Escape":
+        entry_text.set("")
+    elif event.keysym == "Down":
+        try:
+            apps.focus_set()
+            apps.bind("<KeyRelease>", select)
+        except:
+            pass
+        return
+
+    if event.keysym in ["Left", "Right", "Up", "Caps_Lock", "Num_Lock", "Delete", "Shift_L", "Shift_R", "Control_L", "Control_R", "Alt_L", "Alt_R", "Super_L", "Super_R", "Home", "End", "Prior", "Next"]:
+        return
+    search()
+        
+def search():
+    global result, apps, width, height, entry_text, sum
     
-def search(event):
-    global namelist, applist, result, apps, width, height
     kw = entry.get()
     try:
         apps.delete(0, apps.size())
         apps.destroy()
     except:
         pass
-    if kw == ":quit":
+        
+    if kw == ":q":
         root.destroy()
 
-    rekw = kw.replace("*", ".*")
-    rekw = ".*" + rekw.replace("?", ".") + ".*"
-    result = sorted([[namelist[i],  applist[i]] for i in range(len(namelist)) if re.match(rekw.lower(), namelist[i].lower())])
+    result = searcher.getlist(kw)
     sum = len(result)
 
     if kw != "" and sum != 0:            
@@ -58,12 +71,16 @@ def search(event):
         
         apps.place(x = 5, y = 10 + height)
         for i in range(sum):
-            apps.insert(i, result[i][0])
+            apps.insert(i, result[i]["Name"])
     else:
         root.geometry("{}x{}+0+0".format(width, 35))
         
-namelist, applist, result, apps = [], [], [], []
+result, apps = [], []
+searcher = Searcher()
 width, height = 240, 25
+sum = 0
+flag = False
+
 root = Tk()
 
 root.maxsize(width, 10 + height * 21)
@@ -72,9 +89,9 @@ root.geometry("{}x{}+0+0".format(width, 35))
 
 root.title("Linux Quick Launcher")
 
-entry = Entry(root, width = 32)
+entry_text = StringVar()
+entry = Entry(root, width = 32, textvariable = entry_text)
 entry.place(x = 5, y = 5)
-entry.bind("<KeyRelease>", search)
-
-init()
+entry.bind("<KeyRelease>", input)
+entry.focus_set()
 root.mainloop()
