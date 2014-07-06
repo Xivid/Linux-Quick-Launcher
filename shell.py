5d71cea57a4a120bdf2ec5b9672e60cd923ee47f
Xwz7 <Xwszc7@Gmail.com>
2014-07-06 16:51:13 +0800

    update

--- shell.py
+++ shell.py
@@ -5,8 +5,11 @@ import subprocess, webbrowser
 searcher = Searcher()
 print "=========Linux Quick Launcher========="
 print "Type in keywords to search for apps"
+print "Type ID to run apps"
 print "Type ':q' to quit program"
 
+launch_list = []
+length = 0
 while True:
     keyword = raw_input()
     if keyword == "":
@@ -14,20 +17,21 @@ while True:
     elif keyword == ":q":
         print "Thanks for using, bye."
         exit()
+    elif keyword.isdigit() and int(keyword) <= length:
+        print "will execute:" + launch_list[int(keyword)]['Exec']
+        subprocess.Popen(launch_list[int(keyword)]['Exec'], shell = True)
+        launch_list = []
+        length = 0
     else:
         launch_list = searcher.getlist(keyword)
-
+        
         #console display
         if launch_list == []:
             print "No matching results."
         else: #result ranges from 0 to length-1
-             length = len(launch_list)
-             for i in range(0, length):
-                 print str(i) + ". " + launch_list[i]['Name']
-                 print '  ' + launch_list[i]['Comment']
-             choice = eval(raw_input())
-             if choice >= 0:
-                 print 'will execute:' + launch_list[choice]['Exec']
-                 subprocess.Popen(launch_list[choice]['Exec'], shell = True)
-    #console display
+            length = len(launch_list)
+            for i in range(0, length):
+                print str(i) + ". " + launch_list[i]['Name']
+                print '  ' + launch_list[i]['Comment']
+        #console display
 
